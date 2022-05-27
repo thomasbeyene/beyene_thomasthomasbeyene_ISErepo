c0e10480e4e58df7a1b0bd1725bd484b78c0d254
Thomas Beyene <thomas.beyene@student.curtin.edu.au>
2022-05-27 20:03:04 +0800

    functionality 1b commit

--- Modularity.py
+++ Modularity.py
@@ -1,22 +1,41 @@
-def convertString(inString, upper_lower):  # 1
+def convertString(INPUT, upper_lower):  # 1
     #convert string to lower or upper
-    outString = ''
+    OUTPUT = ''
 
     if upper_lower == 'U' or upper_lower == 'u':
-        outString = inString.upper()
+        OUTPUT = INPUT.upper()
     else:
-        outString = inString.lower()
+        OUTPUT = INPUT.lower()
 
-    return outString
+    return OUTPUT
 
 def convertStringOption():  # 1
     convert_type = str(input("Do you want to convert the string to (U)pper or (L)ower case: ")).upper()
 
     if convert_type == 'U' or convert_type == 'L':
-        inString = str(input("Enter string you want to convert: "))
-        outString = convertString(inString, convert_type)
-        print(outString)
+        INPUT = str(input("Enter string you want to convert: "))
+        OUTPUT = convertString(INPUT, convert_type)
+        print(OUTPUT)
     else:
         print("String cannot be converted")
 
+
+def ContainsNumbericValue(INPUT):  # 2
+   # check if input string contains ['0':'9']
+    numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    for i in INPUT:
+        if i in numeric:
+            return True
+
+    return False
+
+
+def ContainsNumbericValueOption():  # 2
+    INPUT = str(input("Enter string you want to check: "))
+
+    if ContainsNumbericValue(INPUT):
+        print("The string contains numeric value.")
+    else:
+        print("The string does not contain numeric value.")
 
