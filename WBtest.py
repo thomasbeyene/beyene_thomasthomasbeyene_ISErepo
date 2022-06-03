f01eb8f0ef6bd74e4aca7f978c1f80377438abdc
Thomas Beyene <thomas.beyene@student.curtin.edu.au>
2022-06-04 06:02:37 +0800

    add IsValidNum testcase for white box testing

--- WBtest.py
+++ WBtest.py
@@ -24,3 +24,27 @@ class WhiteBoxConvertStr(unittest.TestCase):
         sys.stdout = capOut
         sys.stdin = io.StringIO("Interstellar\nO")
         assert "String cannot be converted", False == self.capOut.convertString
+
+class WhiteBoxValidNum(unittest.TestCase):
+    def setUp(self):
+        self.capOut = IsValidNumber
+
+    def testInputValid(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
+        sys.stdin = io.StringIO("3053")
+        assert "3053", True == self.capOut.IsValidNumber
+
+    def testInputInvalid(self):
+        capOut = io.StringIO()
+        sys.stdout = capOut
+        sys.stdin = io.StringIO("Interstellar")
+        assert "Interstellar", False == self.capOut.IsValidNumber
+    
+    def tearDown(self):
+        self.capOut
+
+
+if __name__ == '__main__': 
+    WhiteBoxValidNum()
+    WhiteBoxConvertStr()
